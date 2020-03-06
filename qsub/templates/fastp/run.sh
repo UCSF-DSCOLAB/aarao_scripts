0cdfc01f97d3126d919e3735d2c2875f7ddfd1a6
Arjun Arkal Rao <arkalarjunrao@gmail.com>
2020-03-06 12:18:01 -0800

    Modified fastp to new bash argparse format

--- qsub/templates/fastp/run.sh
+++ qsub/templates/fastp/run.sh
@@ -2,22 +2,42 @@
 set -e
 set -o nounset
 
-source /data/shared/krummellab/ipi/ipi_usr/SOURCE_THIS
+source /krummellab/data1/ipi/ipi_usr/SOURCE_THIS
 
 mkdir /scratch/arrao/fastp_${SAMPLE} && cd /scratch/arrao/fastp_${SAMPLE} 
 trap "{ rm -rf /scratch/arrao/fastp_${SAMPLE} ; }" EXIT
 
-fastp -i ${FQDIR}/${FQPREFIX}1${FQSUFFIX} \
-      -o ${FQPREFIX}1_trimmed${FQSUFFIX} \
-      -I ${FQDIR}/${FQPREFIX}2${FQSUFFIX} \
-      -O ${FQPREFIX}2_trimmed${FQSUFFIX} \
+getExtension() {
+      x=${1##*.}
+      if [ ${x} == "gz" ]
+      then
+            echo .`getExtension ${1%.*}`.${x}
+      elif [ ${x} == "fastq" ]
+      then
+            echo ${x}
+      elif [ ${x} == "fastq" ]
+      then
+            echo ${x}
+      else
+            echo "Could not identify extension"
+            exit 1
+      fi
+}
+
+extension=`getExtension ${FQ1}`
+
+fastp -i ${FQ1} \
+      -I ${FQ2} \
+      -o `basename ${FQ1%${extension}}`_trimmed${extension} \
+      -O `basename ${FQ2%${extension}}`_trimmed${extension} \
       --length_required 20 \
       --adapter_sequence CTGTCTCTTATACACATCT \
       --adapter_sequence_r2 CTGTCTCTTATACACATCT \
       --correction  \
       --trim_poly_g  \
       --thread ${PBS_NUM_PPN} \
-      -j fastp.json \
-      -h fastp.html
+      -j ${SAMPLE}_fastp.json \
+      -h ${SAMPLE}_fastp.html
 
-mv /scratch/arrao/fastp_${SAMPLE} ${OUTDIR}
+mkdir -p ${OUTDIR}
+mv /scratch/arrao/fastp_${SAMPLE} ${OUTDIR}/
--- qsub/templates/fastp/spawn_template.sh
+++ qsub/templates/fastp/spawn_template.sh
@@ -2,46 +2,38 @@
 set -e
 set -o nounset
 
-base_dir=<__BASEDIR__>
-out_dir=<__OUTDIR__>
-log_dir=<_#LOGDIR#_>
-mkdir -p ${log_dir} ${out_dir}
+source $(dirname ${0})/../template_argparse.sh
 
-ls ${base_dir} | while read line
-do
-  if [ ! -d ${base_dir}/${line} ]
-  then
-    continue
-  fi
-  export FQDIR=${base_dir}/${line}
-  if [ $(ls ${FQDIR} | grep -c "fastq\|fq") -ne 2 ]
-  then
-    echo "Cannot process ${FQDIR} since it does not have 2 fastq files."
-    continue
-  fi
-  fq_files=($(ls ${FQDIR} | sort | grep "fastq\|fq"))
-  f_diff=($(cmp -bl <(echo ${fq_files[0]}) <(echo  ${fq_files[1]}) || :))
-  if [ ${#f_diff[@]} -ne 5 ]
-  then
-    echo "Couldn't decompose fastqs in ${FQDIR} into 'PREFIX[12]SUFFIX' because there are > 1 differences between the 2 fastq names."
-    continue
-  elif [ ${f_diff[2]}${f_diff[4]} -ne 12 ]
-  then
-    echo "Couldn't decompose fastqs in ${FQDIR} into 'PREFIX[12]SUFFIX' because the difference between the names was not 1/2."
-    continue
-  fi
-  export FQPREFIX=$(echo ${fq_files[0]} | cut -c -$((${f_diff[0]} - 1)))
-  export FQSUFFIX=$(echo ${fq_files[0]} | cut -c $((${f_diff[0]} + 1))-)
+read -r -d '' REQUIRED_HELPTEXT  << EOF || true
+## REQUIRED PARAMETERS ##
+FQ1       : A path to FQ1
+FQ2       : A path to FQ2
+SAMPLE    : The name of the sample
+OUTDIR    : A folder within which we will place the output dir
+EOF
 
-  export SAMPLE=${line}
+echo "Received the following options:"
+echo "FQ1      : "${FQ1-""}
+echo "FQ2      : "${FQ2-""}
+echo "SAMPLE   : "${SAMPLE-""}
+echo "OUTDIR   : "${OUTDIR-""}
 
-  export OUTDIR=${out_dir}/${SAMPLE}
+if [ ${FQ1-"ERR"} == "ERR" ] || [ ${FQ2-"ERR"} == "ERR" ] || [ ${SAMPLE-"ERR"} == "ERR" ] || [ ${OUTDIR-"ERR"} == "ERR" ]
+then
+    echo -e "\nERROR: Required arguments cannot be empty\n"
+    print_help  
+fi
 
-  qsub -V \
-       -e ${log_dir}/fastp_${SAMPLE}_$(date "+%Y_%m_%d_%H_%M_%S").err \
-       -o ${log_dir}/fastp_${SAMPLE}_$(date "+%Y_%m_%d_%H_%M_%S").out \
-       -N fastp_${SAMPLE} \
-       -l <_#NODEREQS#_> \
-       -l <_#MEMREQS#_> \
-       run.sh
-done
+echo "LOGDIR   : "${LOGDIR}
+echo "NODEREQS : "${NODEREQS}
+echo "MEMREQS  : "${MEMREQS}
+echo -e "\n"
+
+qsub -v "FQ1=$(readlink -f ${FQ1}),FQ2=$(readlink -f ${FQ2}),SAMPLE=${SAMPLE},OUTDIR=$(readlink -f ${OUTDIR})" \
+     -e ${LOGDIR}/fastp_${SAMPLE}_$(date "+%Y_%m_%d_%H_%M_%S").err \
+     -o ${LOGDIR}/fastp_${SAMPLE}_$(date "+%Y_%m_%d_%H_%M_%S").out \
+     -N fastp_${SAMPLE} \
+     -l ${NODEREQS} \
+     -l ${MEMREQS} \
+     $(dirname ${0})/run.sh
+
