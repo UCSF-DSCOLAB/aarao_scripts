ab61983436b36c43449afc4d48986078ed476220
Arjun Arkal Rao <arkalarjunrao@gmail.com>
2020-03-06 12:17:26 -0800

    Modified bwa to new bash argparse format

--- qsub/templates/bwa/run.sh
+++ qsub/templates/bwa/run.sh
@@ -2,13 +2,40 @@
 set -e
 set -o nounset
 
+source /krummellab/data1/ipi/ipi_usr/SOURCE_THIS 
+
 mkdir /scratch/arrao/bwa_${SAMPLE} && cd /scratch/arrao/bwa_${SAMPLE} 
 trap "{ rm -rf /scratch/arrao/bwa_${SAMPLE} ; }" EXIT
 
-mkdir -p ${OUTDIR}
+getRG(){
+ zcat ${1} | 
+ head -1 | 
+ awk -F":" -v SAMPLE=${2} '{print "@RG\\tID:"$3"."$2"."SAMPLE"."$4"\\tSM:"SAMPLE"\\tPU:"$3"."$2"."SAMPLE"."$4"\\tBC:"$NF}'
+}
 
-bwa mem -t ${PBS_NUM_PPN} -v 1 /data/shared/krummellab/ipi/data/refs/bwa/hg38 ${FQDIR}/${FQPREFIX}1${FQSUFFIX} ${FQDIR}/${FQPREFIX}2${FQSUFFIX} > ${SAMPLE}.sam
-samtools view -bS -o ${SAMPLE}.bam ${SAMPLE}.sam && rm ${SAMPLE}.sam
-samtools sort ${SAMPLE}.bam ${SAMPLE}_sorted
-samtools index ${SAMPLE}_sorted.bam
-mv ${SAMPLE}_sorted* ${OUTDIR}/ 
+RG=`getRG ${FQ1} ${SAMPLE}`
+
+bwa mem -t ${PBS_NUM_PPN} \
+        -v 1 \
+        -R ${RG} \
+        ${BWAREF} \
+        ${FQ1} \
+        ${FQ2} > ${SAMPLE}.sam
+
+samtools view -C \
+              -o ${SAMPLE}.cram \
+              -@ ${PBS_NUM_PPN} \
+              -T ${BWAREF}.fa \
+              ${SAMPLE}.sam \
+              && rm ${SAMPLE}.sam
+
+samtools sort ${SAMPLE}.cram \
+              -@ ${PBS_NUM_PPN} \
+              --reference ${BWAREF}.fa \
+              -o ${SAMPLE}_sorted.cram \
+              && rm ${SAMPLE}.cram
+
+samtools index ${SAMPLE}_sorted.cram
+
+mkdir -p ${OUTDIR}
+mv ${SAMPLE}_sorted.cram ${SAMPLE}_sorted.cram.crai ${OUTDIR}/
