a008d23e0b245f8c6ed49f32fd851a4e985cf694
Arjun Arkal Rao <arkalarjunrao@gmail.com>
2020-03-17 18:58:22 -0700

    Add DepthOfCoverage

---
+++ qsub/templates/DepthOfCoverage/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+set -o nounset
+module load CBC gatk/${GATKVERSION}
+
+mkdir /scratch/arrao/DepthOfCoverage_${SAMPLE} && cd /scratch/arrao/DepthOfCoverage_${SAMPLE} 
+trap "{ rm -rf /scratch/arrao/DepthOfCoverage_${SAMPLE} /scratch/arrao/${SAMPLE}_javatmp ; }" EXIT
+
+out_dir=$(dirname ${SAMFILE})
+
+if [ ${INTERVALSFILE-"EMPTY"} == "NONE" ]
+then
+    INTERVALSTRING=" "
+else
+    INTERVALSTRING="--intervals ${INTERVALSFILE}"
+fi
+
+java -Djava.io.tmpdir=/scratch/arrao/${SAMPLE}_javatmp -Xmx${MEMORY}g -jar $GATK_HOME/GenomeAnalysisTK.jar \
+    -T DepthOfCoverage \
+    -I ${SAMFILE} \
+    ${INTERVALSTRING} \
+    --reference_sequence ${GENOMEREF} \
+    --out ${SAMPLE} \
+    -nt ${PBS_NUM_PPN} \
+    --omitIntervalStatistics \
+    --omitDepthOutputAtEachBase \
+    --summaryCoverageThreshold 10 \
+    --summaryCoverageThreshold 25 \
+    --summaryCoverageThreshold 50 \
+    --summaryCoverageThreshold 100 
+
+mv /scratch/arrao/DepthOfCoverage_${SAMPLE}/ ${out_dir}/DepthOfCoverage/
---
+++ qsub/templates/DepthOfCoverage/spawn.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+set -o nounset
+
+source $(dirname ${0})/../template_argparse.sh
+
+read -r -d '' REQUIRED_HELPTEXT  << EOF || true
+## REQUIRED PARAMETERS ##
+SAMFILE   : A path to the aligned sam/bam/cram file
+SAMPLE    : The name of the sample
+EOF
+
+read -r -d '' LOCAL_OPTIONAL_HELPTEXT  << EOF || true
+## OPTIONAL PARAMETERS ##
+INTERVALFILE   : An intervals file to calculate over, use NONE for no itervals (/krummellab/data1/ipi/data/refs/hg38_files/GRCh38_CCDS_sorted.bed.gz)
+GATKVERSION    : The version of gatk to use (3.8.0)
+GENOMEREF      : A path to the reference genome fasta (/krummellab/data1/ipi/data/refs/hg38_files/hg38.fa)
+EOF
+
+if [ ${GATKVERSION-"EMPTY"} == "EMPTY" ]
+then
+    GATKVERSION="3.8.0"
+fi
+
+if [ ${GENOMEREF-"EMPTY"} == "EMPTY" ]
+then
+    GENOMEREF="/krummellab/data1/ipi/data/refs/hg38_files/hg38.fa"
+fi
+
+echo "Received the following options:"
+echo "SAMPLE   : "${SAMPLE-""}
+echo "SAMFILE  : "${SAMFILE-""}
+echo "INTERVALFILE  : "${INTERVALFILE-""}
+echo "GATKVERSION  : "${GATKVERSION-""}
+echo "GENOMEREF  : "${GENOMEREF-""}
+
+if [ ${SAMPLE-"ERR"} == "ERR" ] || [ ${SAMFILE-"ERR"} == "ERR" ]
+then
+    echo -e "\nERROR: Required arguments cannot be empty\n"
+    print_help  
+fi
+
+echo "LOGDIR   : "${LOGDIR}
+echo "NODEREQS : "${NODEREQS}
+echo "MEMREQS  : "${MEMREQS}
+echo -e "\n"
+
+MEMORY=`echo "$(echo ${MEMREQS} | sed 's/[^0-9]*//g')*0.9 / 1" | bc`
+
+qsub -v "SAMFILE=$(readlink -f ${SAMFILE}),SAMPLE=${SAMPLE},GENOMEREF=$(readlink -f ${GENOMEREF}),MEMORY=${MEMORY},GATKVERSION=${GATKVERSION}" \
+     -e ${LOGDIR}/DepthOfCoverage_${SAMPLE}_$(date "+%Y_%m_%d_%H_%M_%S").err \
+     -o ${LOGDIR}/DepthOfCoverage_${SAMPLE}_$(date "+%Y_%m_%d_%H_%M_%S").out \
+     -N DepthOfCoverage_${SAMPLE} \
+     -l ${NODEREQS} \
+     -l ${MEMREQS} \
+     $(dirname ${0})/run.sh
