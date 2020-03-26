fb8399c622fd4d13058ba76e1031413b0c190e55
Arjun Arkal Rao <arkalarjunrao@gmail.com>
2020-03-25 18:03:59 -0700

    Fixed issues with SE bwa Added options to provide RG items via CLI

--- qsub/templates/bwa/run.sh
+++ qsub/templates/bwa/run.sh
@@ -7,13 +7,62 @@ source /krummellab/data1/ipi/ipi_usr/SOURCE_THIS
 mkdir /scratch/arrao/bwa_${SAMPLE} && cd /scratch/arrao/bwa_${SAMPLE} 
 trap "{ rm -rf /scratch/arrao/bwa_${SAMPLE} ; }" EXIT
 
+# This is old and will eb deleted soon.
 getRG(){
  zcat ${1} | 
- head -1 | 
- awk -F":" -v SAMPLE=${2} '{print "@RG\\tID:"$3"."$2"."SAMPLE"."$4"\\tPL:ILLUMINA\\tSM:"SAMPLE"\\tPU:"$3"."$2"."SAMPLE"."$4"\\tBC:"$NF}'
+ awk -F":" -v SAMPLE=${2} '{print "@RG\\tID:"$3"."$2"."SAMPLE"."$4"\\tPL:ILLUMINA\\tSM:"SAMPLE"\\tPU:"$3"."$4"\\tBC:"$NF; exit}'
 }
 
-RG=`getRG ${FQ1} ${SAMPLE}`
+# This will return the 5 RG items we are interested in, in the fixed order
+# ID PL SM PU BC
+get_RG_items(){
+  first_record=$(head -n 1 < <(zcat ${FQ1} 2>/dev/null) | grep "@" )
+
+  BC=($(head -n 4000 < <(zcat ${FQ1} 2>/dev/null) | grep "@" | awk '{print $2}' | sort | uniq -c | sort -k1n | tail -1))
+
+  if [[ ${BC[0]} < 750 ]]
+  then
+    echo "WARNING: Less than 75% of the top 1000 reads have the most popular barcode (${BC[1]})" >&2
+  fi
+
+  echo ${first_record} | 
+           awk -F":" -v SAMPLE=${2} -v BARCODE=${BC[1]/*:/} '{print $3"."$4" ILLUMINA "SAMPLE" "$3"."$4" "BARCODE}'
+}
+
+RG_items=($(echo $(get_RG_items ${FQ1} ${SAMPLE})))
+
+if [ ${RGID} == "EMPTY" ]
+then
+    RGID=${RG_items[0]}
+fi
+
+if [ ${RGPL} == "EMPTY" ]
+then
+    RGPL=${RG_items[1]}
+fi
+
+if [ ${RGSM} == "EMPTY" ]
+then
+    RGSM=${RG_items[2]}
+fi
+
+if [ ${RGPU} == "EMPTY" ]
+then
+    RGPU=${RG_items[3]}
+fi
+
+if [ ${RGBC} == "EMPTY" ]
+then
+    RGBC=${RG_items[4]}
+fi
+
+RG="@RG\\tID:"$RGID"\\tPL:${RGPL}\\tSM:${RGSM}\\tPU:${RGPU}\\tBC:${RGBC}"
+
+
+if [ $(basename ${FQ2}) == "EMPTY" ]
+then
+      FQ2=" "
+fi
 
 bwa mem -t ${PBS_NUM_PPN} \
         -v 1 \
--- qsub/templates/bwa/spawn.sh
+++ qsub/templates/bwa/spawn.sh
@@ -7,15 +7,25 @@ source $(dirname ${0})/../template_argparse.sh
 read -r -d '' REQUIRED_HELPTEXT  << EOF || true
 ## REQUIRED PARAMETERS ##
 FQ1       : A path to FQ1
-FQ2       : A path to FQ2
 SAMPLE    : The name of the sample
 OUTDIR    : A folder within which we will place the output dir
 EOF
 
 read -r -d '' LOCAL_OPTIONAL_HELPTEXT  << EOF || true
 ## OPTIONAL PARAMETERS ##
-BWAREF   : A path to a bwa reference (/krummellab/data1/ipi/data/refs/bwa/hg38)
+FQ2       : A path to FQ2
+BWAREF    : A path to a bwa reference (default=/krummellab/data1/ipi/data/refs/bwa/hg38)
+RGID      : Read Group ID (default=PARSED_FROM_1ST_READ to be FLOWCELL_ID.LANE_NUM)
+RGPL      : Read Group Platform (default=ILLUMINA)
+RGSM      : Read Group Sample (default=<SAMPLE>)
+RGPU      : Read Group Platform Unit (default=PARSED_FROM_1ST_READ to be FLOWCELL_ID.LANE_NUM)
+RGBC      : Read Group Barcode (default=PARSED_FROM_1ST_1000_READS and warns if < 75% are concordant)
 EOF
+
+if [ ${FQ2-"EMPTY"} == "EMPTY" ]
+then
+    FQ2="EMPTY"
+fi
 
 if [ ${BWAREF-"EMPTY"} == "EMPTY" ]
 then
@@ -24,13 +34,45 @@ else
     BWAREF=${BWAREF%.fa}
 fi
 
+if [ ${RGID-"EMPTY"} == "EMPTY" ]
+then
+    RGID="EMPTY"
+fi
+
+if [ ${RGPL-"EMPTY"} == "EMPTY" ]
+then
+    RGPL="EMPTY"
+fi
+
+if [ ${RGSM-"EMPTY"} == "EMPTY" ]
+then
+    RGSM="EMPTY"
+fi
+
+if [ ${RGPU-"EMPTY"} == "EMPTY" ]
+then
+    RGPU="EMPTY"
+fi
+
+if [ ${RGBC-"EMPTY"} == "EMPTY" ]
+then
+    RGBC="EMPTY"
+fi
+
+
+
 echo "Received the following options:"
 echo "FQ1      : "${FQ1-""}
 echo "FQ2      : "${FQ2-""}
 echo "SAMPLE   : "${SAMPLE-""}
 echo "OUTDIR   : "${OUTDIR-""}
+echo "RGID     : "${RGID-""}
+echo "RGPL     : "${RGPL-""}
+echo "RGSM     : "${RGSM-""}
+echo "RGPU     : "${RGPU-""}
+echo "RGBC     : "${RGBC-""}
 
-if [ ${FQ1-"ERR"} == "ERR" ] || [ ${FQ2-"ERR"} == "ERR" ] || [ ${SAMPLE-"ERR"} == "ERR" ] || [ ${OUTDIR-"ERR"} == "ERR" ]
+if [ ${FQ1-"ERR"} == "ERR" ] || [ ${SAMPLE-"ERR"} == "ERR" ] || [ ${OUTDIR-"ERR"} == "ERR" ]
 then
     echo -e "\nERROR: Required arguments cannot be empty\n"
     print_help  
@@ -42,7 +84,16 @@ echo "NODEREQS : "${NODEREQS}
 echo "MEMREQS  : "${MEMREQS}
 echo -e "\n"
 
-qsub -v "FQ1=$(readlink -f ${FQ1}),FQ2=$(readlink -f ${FQ2}),SAMPLE=${SAMPLE},OUTDIR=$(readlink -f ${OUTDIR}),BWAREF=$(readlink -f ${BWAREF})" \
+qsub -v "FQ1=$(readlink -e ${FQ1}),\
+         FQ2=$(readlink -f ${FQ2}),\
+         SAMPLE=${SAMPLE},\
+         OUTDIR=$(readlink -f ${OUTDIR}),\
+         BWAREF=$(readlink -f ${BWAREF}),\
+         RGID=${RGID},\
+         RGPL=${RGPL},\
+         RGSM=${RGSM},\
+         RGPU=${RGPU},\
+         RGBC=${RGBC}" \
      -e ${LOGDIR}/demuxlet_${SAMPLE}_$(date "+%Y_%m_%d_%H_%M_%S").err \
      -o ${LOGDIR}/demuxlet_${SAMPLE}_$(date "+%Y_%m_%d_%H_%M_%S").out \
      -N bwa_${SAMPLE} \
