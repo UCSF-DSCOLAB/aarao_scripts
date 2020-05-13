6f94806832fbb6b02bad73a206627ab79d5a580a
Arjun Arkal Rao <arkalarjunrao@gmail.com>
2020-05-13 15:37:22 -0700

    Updated get_10x_multiplet_rate.R to have helptext

--- R/get_10x_multiplet_rate.R
+++ R/get_10x_multiplet_rate.R
@@ -1,6 +1,12 @@
 library(assertthat)
 
 args = commandArgs(trailingOnly=TRUE)
+if (length(args) == 0 | "-h" %in% args | "--help" %in% args){
+    print('USAGE: Rscript get_10x_multiplet_rate.R cells_loaded1 cells_loaded2 cells ... cell_loadedn')
+    print('where cells_loaded1,2..n are the number of cells loaded in the well.')
+    quit("no")
+}
+
 vals <- as.numeric(args)
 
 x = assert_that(all(args[c(1:length(args))] > 0), msg="args must be greater than 0")
