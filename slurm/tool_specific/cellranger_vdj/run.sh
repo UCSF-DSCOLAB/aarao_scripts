2f04a1c073886aea4e3d8e1adab1a91702b9c06b
Arjun Arkal Rao <arkalarjunrao@gmail.com>
2021-08-19 12:14:08 -0700

    Add cellranger vdj to the slurm scripts
    Add cellranger vdj to the slurm scripts using cellranger 6.0.2 by default
    and the correct references

---
+++ slurm/tool_specific/cellranger_vdj/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+set -o nounset
+
+if [[ ${TMPDIR-"EMPTY"} ==  "EMPTY" ]]
+then
+    TMPDIR=/scratch/${USER}/$(head -10 /dev/null | md5sum | cut -c 1-10)
+    trap "{ rm -rf ${TMPDIR} ; }" EXIT
+fi
+
+mkdir ${TMPDIR}/cellranger_vdj_${SAMPLE}
+cd ${TMPDIR}/cellranger_vdj_${SAMPLE}
+
+bindmount_string=$(python3 ${COLLAPSEDIRSCRIPT} --prefixB $(dirname ${FASTQDIR}) ${REFERENCE} ${PWD})
+
+singularity exec \
+            ${bindmount_string} \
+            --pwd ${PWD} \
+            ${CONTAINER} cellranger vdj \
+                                --id=${SAMPLE} \
+                                --fastqs=${FASTQDIR} \
+                                --reference=${REFERENCE} \
+                                --chain=${CHAIN} \
+                                --localcores=${SLURM_CPUS_PER_TASK} \
+                                --localmem=${USABLEMEMORY}
+
+if [[ -d ${OUTDIR} ]]
+then
+    mv ${TMPDIR}/cellranger_vdj_${SAMPLE}/${SAMPLE}/outs ${OUTDIR}/${SAMPLE}_vdj
+else
+    mkdir -p $(dirname ${OUTDIR})
+    mv ${TMPDIR}/cellranger_vdj_${SAMPLE}/${SAMPLE}/outs ${OUTDIR}
+fi
